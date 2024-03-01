79bf82e51fb6b4730f0238c0a8f8709b2b0f7a73
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 14:03:26 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -5,7 +5,7 @@ const plugin = {
     bot.onText(/^\/sc$/, (msg) => {
       const From = msg.chat.id;
       const user = msg.from;
-      const caption = `Hi ${user.first_name}! This Bot Uses Script From`;
+      const caption = `Hi ${user.first_name}! Follow my Instagram`;
       const replyMarkup = {
         reply_markup: {
           inline_keyboard: [
