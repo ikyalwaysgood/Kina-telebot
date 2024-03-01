2d1b7ce3fc0ec5205cd87f116c8f631ebd89f0cb
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 13:57:12 +0700

    Update download-douyin.js

--- plugins/download-douyin.js
+++ plugins/download-douyin.js
@@ -32,7 +32,7 @@ const plugin = {
         const replyMarkup = {
           reply_markup: {
             inline_keyboard: [
-              [{ text: 'Script Bot', url: buttonUrl }],
+              [{ text: 'Follow Instagram', url: buttonUrl }],
               [{ text: 'Audio', url: audio }],
             ],
           },
