b85b7796d70647b69bfd2a95750c1d66e51d557a
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 13:57:28 +0700

    Update download-douyinslide.js

--- plugins/download-douyinslide.js
+++ plugins/download-douyinslide.js
@@ -27,7 +27,7 @@ const plugin = {
         const replyMarkup = {
           reply_markup: {
             inline_keyboard: [
-              [{ text: 'Script Bot', url: buttonUrl }],
+              [{ text: 'Follow Instagram', url: buttonUrl }],
               [{ text: 'Audio', url: play }],
             ],
           },
