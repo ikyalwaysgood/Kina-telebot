7617c383015e8e6aa7098162fbf617bb942d35b5
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-03-01 14:00:07 +0700

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -19,7 +19,7 @@ const plugin = {
       const replyMarkup = {
         reply_markup: {
           inline_keyboard: [
-            [{ text: 'Script Bot', url: buttonUrl }],
+            [{ text: 'Follow Instagram', url: buttonUrl }],
           ],
         },
       };
